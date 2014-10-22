ac30ac4719bb458711824faa0d91a73f65dc104c
asonas <hzw1258@gmail.com>
2014-10-23 01:33:03 +0900

    Update

--- rsync.sh
+++ rsync.sh
@@ -1,2 +1,2 @@
 rsync --checksum -av /home/asonas/djbu-client/music/ amanusa:/var/www/namanas/files
-curl -d token=xoxp-2314826254-2314826256-2370224920-6085a7 -d channel=C0298QA7Q -d text="Successful syncronization. http://namanas.ason.as/" -d username=asoNAS https://slack.com/api/chat.postMessage
+curl -d token=$SLACK_TOKEN -d channel=C0298QA7Q -d text="@asonas @namamana Successful syncronization. http://namanas.ason.as/" -d username=asoNAS https://slack.com/api/chat.postMessage
